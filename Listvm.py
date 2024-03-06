b65a3cb5e0615c4187b4eba7e178d7b8c9df32d8
kkalal738 <37746754+kkalal738@users.noreply.github.com>
2024-03-06 15:28:28 +0530

    Update Listvm.py

--- Listvm.py
+++ Listvm.py
@@ -1,1 +1,22 @@
 
+import subprocess
+
+def list_vm():
+    proj = "your-project-id"
+    output = subprocess.run(['gcloud', 'asset', 'search-all-resources', '--scope=projects/'+proj, '--asset-types=compute.googleapis.com/Instance', '--read-mask=name,project,location,state','--query=labels.autostartstop:yes'], capture_output=True, text=True)
+    # Parse the output
+    instances = []
+    for item in output.stdout.strip().split('---\n'):
+        if item.strip():
+            instance = {}
+            for line in item.strip().split('\n'):
+                key, value = line.split(': ', 1)
+                instance[key] = value
+            instances.append(instance)
+
+    # Print the instances
+    for instance in instances:
+        print(instance)
+
+# Run the function
+list_vm()
